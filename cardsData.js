4626a0c3ae85dc0741ef3d5812be8158a21fd9de
catkane-doodles <tanpeikai@hotmail.com>
2020-10-15 16:46:10 +0800

    Initial Commit

---
+++ cardsData.js
@@ -0,0 +1,103 @@
+let lorem =
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere facilis modi provident molestiae sapiente ducimus vero nulla eligendi eius, tempora, magni at velit consequatur exercitationem id vel architecto deserunt placeat!";
+let markup = {
+  title: "Markup",
+  cards: [
+    {
+      avatar: "https://image.flaticon.com/icons/png/512/1216/1216733.png",
+      name: "HTML5",
+      subtitle: "div div div div div div div",
+      documentation: "#",
+      content: lorem,
+    },
+  ],
+};
+
+let scripting = {
+  title: "Scripting",
+  cards: [
+    {
+      avatar: "",
+      name: "JavaScript",
+      subtitle: "Script that sh*t",
+      documentation: "#",
+      content: lorem,
+    },
+    {
+      avatar: "",
+      name: "JQuery",
+      subtitle: "Harder, but faster",
+      documentation: "#",
+      content: lorem,
+    },
+  ],
+};
+
+let styling = {
+  title: "Styling",
+  cards: [
+    {
+      avatar: "",
+      name: "CSS",
+      subtitle: "Make that sh*t pretty",
+      documentation: "#",
+      content: lorem,
+    },
+    {
+      avatar: "",
+      name: "SASS",
+      subtitle: "Make that sh*t pretty",
+      documentation: "#",
+      content: lorem,
+    },
+
+    {
+      avatar: "",
+      name: "Bulma",
+      subtitle: "Style that sh*t (easier)",
+      documentation: "#",
+      content: lorem,
+    },
+    {
+      avatar: "",
+      name: "Bootstrap",
+      subtitle: "Style that sh*t (standard)",
+      documentation: "#",
+      content: lorem,
+    },
+    {
+      avatar: "",
+      name: "Materialize",
+      subtitle: "Style that sh*t (standard)",
+      documentation: "#",
+      content: lorem,
+    },
+  ],
+};
+
+let frameworks = {
+  title: "Frameworks",
+  cards: [
+    {
+      avatar: "",
+      name: "Vue.js",
+      subtitle: "Link that sh*t",
+      documentation: "#",
+      content: lorem,
+    },
+    {
+      avatar: "",
+      name: "REACT",
+      subtitle: "Link that sh*t",
+      documentation: "#",
+      content: lorem,
+    },
+    {
+      avatar: "",
+      name: "Angular",
+      subtitle: "Link that sh*t",
+      documentation: "#",
+      content: lorem,
+    },
+  ],
+};
---
+++ v.js
@@ -0,0 +1,6 @@
+var categories = new Vue({
+  el: "#categories",
+  data: {
+    categories: [markup, scripting, styling, frameworks],
+  },
+});
